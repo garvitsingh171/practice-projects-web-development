b5e21d4fbdd3292eea40fa9261a87c9daf70ea2b
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2025-09-26 16:09:47 +0530

    Modified JS file just little priority change

--- Dynamic-Website-Builder/script.js
+++ Dynamic-Website-Builder/script.js
@@ -1,32 +1,36 @@
-const card = document.getElementById('container');
-
 function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    const card = document.getElementById('container');
+    card.style.display = 'flex';
+    card.style.flexDirection = 'coloumn';
+
+    const thumb = document.createElement('img')
+
+    thumb.src = thumbnail;
+    thumb.style.height = '200px'
+    thumb.style.width = '350px'
+    card.appendChild(thumb)
+
     const cardTitle = document.createElement('div')
 
     cardTitle.textContent = title;
     card.appendChild(cardTitle)
 
-    const thumb = document.createElement('img')
-
-    thumb.src = thumbnail;
-    card.appendChild(thumb)
-
-    const period = document.createElement('span')
+    const period = document.createElement('div')
 
     period.textContent = duration;
     card.appendChild(period)
 
-    const impress = document.createElement('span')
+    const impress = document.createElement('div')
 
     impress.textContent = views;
     card.appendChild(impress)
 
-    const channel = document.createElement('span')
+    const channel = document.createElement('div')
 
     channel.textContent = cName;
     card.appendChild(channel)
 
-    const age = document.createElement('span')
+    const age = document.createElement('div')
 
     age.textContent = `${monthsOld} year old`
     card.appendChild(age)
